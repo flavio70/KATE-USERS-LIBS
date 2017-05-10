1905c92e5e461303fbe7e9a7945cf14be8159844
luigir <luigir@it052016.it.alcatel-lucent.com>
2017-05-10 09:34:35 +0200

    Fixed 2 bugs in verify_conn_br_tps536x and setup_vc_conc functions

--- kateUsrLibs/luigir/conn_lib.py
+++ kateUsrLibs/luigir/conn_lib.py
@@ -114,8 +114,8 @@ def formatDate():
     return(formatDate)
     
 
-def select_from(rate, au_type, ne_s1):
-    conn_from = rate + au_type + "-" + ne_s1 + "-1"
+def select_from(rate, au_type, ne_s1, au_num = "-1"):
+    conn_from = rate + au_type + "-" + ne_s1 + au_num
     return(conn_from)
 
 
@@ -741,13 +741,13 @@ def setup_vc_conc(NE1, ONT1, ONT2, ONT1_P1, ONT2_P1, rate, vc_type, au_type, ne_
     elif((rate == "STM64") and (vc_type == "VC416C")):
         hostruct_s1 = "1xAU416C-48xAU4;"
         if(n_conn == 1):
-            hostruct_s2 = "1xAU44C-60xAU4;"
+            hostruct_s2 = "1xAU416C-48xAU4;"
         else:
             hostruct_s2 = "4xAU416C"
         conc_factor = 16
     else:
         hostruct_s1= "1xAU464C;"
-        hostruct_s1= "1xAU464C;"
+        hostruct_s2= "1xAU464C;"
         conc_factor = 64
     
     tl1_command= "ED-" + rate + "::" + rate + "-" + ne_s1 + "::::" + "CMDMDE=FRCD,HOSTRUCT=" + hostruct_s1
@@ -949,7 +949,7 @@ def verify_conn_br_tps536x(self, NE1, ONT1, ONT2, ONT1_P1, ONT2_P1, rate, vc_typ
             default_vc_type(NE1, ONT1, ONT2, ONT1_P1, ONT2_P1, rate, vc_type, au_type, ne_s1, ne_s2)
         elif(vc_type == "VC416C"):
             conc_factor = setup_vc_conc(NE1, ONT1, ONT2, ONT1_P1, ONT2_P1, rate, vc_type, au_type, ne_s1, ne_s2, 4)
-            test_result = test_conn_br(self, NE1, ONT1, ONT2, ONT1_P1, ONT2_P1, rate, vc_type, au_type, ne_s1, ne_s2, 16, conc_factor)
+            test_result = test_conn_br(self, NE1, ONT1, ONT2, ONT1_P1, ONT2_P1, rate, vc_type, au_type, ne_s1, ne_s2, 4, conc_factor)
             default_vc_type(NE1, ONT1, ONT2, ONT1_P1, ONT2_P1, rate, vc_type, au_type, ne_s1, ne_s2)
         elif(vc_type == "VC464C"):
             conc_factor = setup_vc_conc(NE1, ONT1, ONT2, ONT1_P1, ONT2_P1, rate, vc_type, au_type, ne_s1, ne_s2, 1)
